64da64cbda62193cd07abca50a1cde4575312110
MChau105 <bdmchau10005@gmail.com>
2021-08-14 19:28:04 +0700

    Chau

--- Network_UDP/src/Service/UDP/UDP_Client.java
+++ Network_UDP/src/Service/UDP/UDP_Client.java
@@ -5,10 +5,43 @@
  */
 package Service.UDP;
 
+import java.io.IOException;
+import java.net.*;
+
 /**
- *
  * @author Admin
  */
 public class UDP_Client {
-    
+    private DatagramSocket socket;
+    private InetAddress address;
+
+    private byte[] bufferByte;
+
+    public UDP_Client() throws SocketException, UnknownHostException {
+        socket = new DatagramSocket();
+        address = InetAddress.getByName("localhost");
+    }
+
+    public String send(String msg) throws IOException {
+        bufferByte = msg.getBytes();
+        
+        DatagramPacket packet = new DatagramPacket(bufferByte, bufferByte.length, address, 8080);
+        
+        socket.send(packet);
+
+        packet = new DatagramPacket(bufferByte, bufferByte.length);
+
+        socket.receive(packet);
+
+
+        String receivedFromServer = new String(packet.getData(), 0, packet.getLength());
+
+
+
+        return receivedFromServer;
+    }
+
+    public void close() {
+        socket.close();
+    }
 }
--- Network_UDP/src/Service/UDP/UDP_Server.java
+++ Network_UDP/src/Service/UDP/UDP_Server.java
@@ -5,10 +5,56 @@
  */
 package Service.UDP;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
 /**
  *
  * @author Admin
  */
-public class UDP_Server {
-    
+public class UDP_Server extends Thread {
+    private DatagramSocket socket;
+    private boolean isRunning;
+    private byte[] buf = new byte[256];
+
+    public UDP_Server() throws SocketException {
+        socket = new DatagramSocket(8080);
+    }
+
+    public void run() {
+        isRunning = true;
+
+        while (isRunning) {
+            DatagramPacket packet = new DatagramPacket(buf, buf.length);
+            try {
+                socket.receive(packet);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            InetAddress address = packet.getAddress();
+            int port = packet.getPort();
+
+            packet = new DatagramPacket(buf, buf.length, address, port);
+            String receivedFromClient = new String(packet.getData(), 0, packet.getLength());
+
+            System.out.println("Server nhận từ client: " + receivedFromClient);
+
+            if (receivedFromClient.equals("end")) {
+                isRunning = false;
+                continue;
+            }
+
+            try {
+                socket.send(packet);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        socket.close();
+    }
 }
